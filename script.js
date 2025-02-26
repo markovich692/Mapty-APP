6e6e7d15a061db4b43ad5a8029df9d79cab9c7c8
Marc Diallo <mark.devguru@outlook.com>
2025-02-25 20:50:15 -0600

    adds pin to the map on click

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+      let coords = [latitude, longitude];
 
+      //sets the view to the current location
       const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -24,14 +25,20 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-      });
-
       L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         .openPopup();
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        coords = [lat, lng];
+
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+      });
     },
     function () {
       alert('Unable to get location');
