ac879772b31e0ab0e0eac2b07b9403f1b9c385de
Marc Diallo <mark.devguru@outlook.com>
2025-02-26 10:49:09 -0600

    clears input on submit

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map;
+let mapEvent;
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -18,47 +20,56 @@ if (navigator.geolocation)
       let coords = [latitude, longitude];
 
       //sets the view to the current location
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
+      //Handling events on map using the map object
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
 
         //Modifies the form according to the type of activities
         //if Cycling---->Elev Gain on the form
         //if Running---->Cadence on the form
-
-        form.addEventListener('submit', function (e) {
-          e.preventDefault();
-
-          //OnSubmit---->Adds marker to the page && adds the activity to the list
-
-          const { lat, lng } = mapEvent.latlng;
-          L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(
-              L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup',
-              })
-            )
-            .setPopupContent('Workout')
-            .openPopup();
-        });
       });
     },
     function () {
       alert('Unable to get location');
     }
   );
+
+//FORM
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //OnSubmit---->Adds marker to the page && adds the activity to the list
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+
+  //Clear input on submit
+  inputDistance.value = '';
+  inputDuration.value = '';
+  inputCadence.value = '';
+  inputElevation.value = '';
+});
 
 // let formattedCurrency = new Intl.NumberFormat(navigator.language, {
 //   style: 'currency',
